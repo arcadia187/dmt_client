ba361363edf6db1ab5016926b516ff296f434558
Prashantt423 <84055497+Prashantt423@users.noreply.github.com>
2022-10-28 19:47:44 +0530

    register feature

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store, persistor } from "./constants/configStore.js";
 import { PersistGate } from "redux-persist/integration/react";
 // import "bootstrap/dist/css/bootstrap.min.css";
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
     </PersistGate>
-  </Provider>,
-
-  document.getElementById("root")
+  </Provider>
 );
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <PersistGate loading={null} persistor={persistor}>
+//       <App />
+//     </PersistGate>
+//   </Provider>,
+
+//   document.getElementById("root")
+// );
