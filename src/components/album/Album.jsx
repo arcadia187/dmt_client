3be428c21614df782717052cf82436734980328b
Prashantt423 <84055497+Prashantt423@users.noreply.github.com>
2022-11-10 16:46:35 +0530

    fonts added

---
+++ src/components/album/Album.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Album() {
+  return (
+    <div>
+      <div className="tracks">
+        <h3>Track List</h3>
+        <p className="song">1.</p>
+      </div>
+    </div>
+  );
+}
